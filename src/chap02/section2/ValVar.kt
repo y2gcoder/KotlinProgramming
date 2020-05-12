9d16e96f3a89e1b15cac48980ed309d24a97bf76
yang <y2gcoder@gmail.com>
2020-05-12 18:01:45 +0900

    02-2 변수와 자료형 24분 06초까지

---
+++ src/chap02/section2/ValVar.kt
@@ -0,0 +1,17 @@
+package chap02.section2
+
+fun main() {
+    val number: Int = 10
+    val number2 = 20
+    val numberOfBooks: Int = 30
+    val init: Int = 10
+
+    var language = "English"
+    language = "Korean"
+
+
+
+    println("number: $number, number2: $number2, init: $init")
+    println("language: $language, Books: $numberOfBooks")
+
+}
